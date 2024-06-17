55498d43f7f634eac8bd184314c52aba2b4fba01
Alexsandra <sm1991611@gmail.com>
2024-06-17 18:36:13 +0700

    create age

--- src/Info.java
+++ src/Info.java
@@ -2,5 +2,6 @@ public class Info {
     public void ShowInfo(){
         System.out.println("Name: Alexandra");
         System.out.println("First name: Matveeva");
+        System.out.println("Age: 21");
     }
 }
