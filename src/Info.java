9626117ebb6bcf627cb6dbb159a4a16701ebc7a1
Alexsandra <sm1991611@gmail.com>
2024-06-17 18:44:06 +0700

    delete info

--- src/Info.java
+++
@@ -1,7 +0,0 @@
-public class Info {
-    public void ShowInfo(){
-        System.out.println("Name: Alexandra");
-        System.out.println("First name: Matveeva");
-        System.out.println("Age: 21");
-    }
-}
